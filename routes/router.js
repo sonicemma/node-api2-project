b45b3eb2b591dcec1a20b3f95d4f8b85f845ae40
emma <sonicjane2020@gmail.com>
2020-04-08 00:02:35 -0700

    added endpoints in router.js

--- routes/router.js
+++ routes/router.js
@@ -2,4 +2,33 @@ const express = require('express');
 const Posts = require('../data/db');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send(`hello from router`);
+})
+
+router.get('/:id', (req, res) => {
+    
+})
+
+router.get('/:id/comments', (req, res) => {
+    
+})
+
+router.post('/', (req, res) => {
+
+})
+
+router.post('/:id/comments', (req, res) => {
+
+})
+
+router.put('/:id', (req, res) => {
+    
+})
+
+router.delete('/:id', (req, res) => {
+    
+})
+
+
 module.exports = router;
