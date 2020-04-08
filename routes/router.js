05bbe6bef2e0662102455ca8075f8708ff69f61e
emma <sonicjane2020@gmail.com>
2020-04-08 01:40:54 -0700

    coded router.post, it works

--- routes/router.js
+++ routes/router.js
@@ -32,10 +32,33 @@ router.get('/:id', (req, res) => {
 })
 
 router.get('/:id/comments', (req, res) => {
+    Posts.findPostComments(req.params.id)
+    .then((comments) => {
+        if (comments) {
+            res.status(200).json(comments);
+        } else {
+            res.status(404).json({message: 'Comments not found'})
+        }
+    })
+    .catch(error =>{
+        console.log(err.message, err);
+        res.status(500).json({message: 'Error retrieving comments'})
+    })
 })
 
 router.post('/', (req, res) => {
-
+    Posts.insert(req.body)
+    .then((post) => {
+        if (!post.title || !post.contents) {
+            res.status(400).json({message: 'Add title and content for post'})
+        } else {
+            res.status(201).json(post);
+        }
+    })
+    .catch((error) => {
+        console.log(error.message, error);
+        res.status(500).json({message: 'Error adding post'})
+    })
 })
 
 router.post('/:id/comments', (req, res) => {
