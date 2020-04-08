4964d34d08ab148d91540bb88d849840899eb714
emma <sonicjane2020@gmail.com>
2020-04-07 23:27:52 -0700

    set up very basic file structure

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+
+server.listen(4000, () => {
+    console.log("\n*** Server Running on http://localhost:4000 ***\n");
+  });
