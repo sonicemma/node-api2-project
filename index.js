b967c8f47d3ed1142badf07d23d8641b0a1163f7
emma <sonicjane2020@gmail.com>
2020-04-07 23:59:39 -0700

    cleaned up index.js, code mostly set in place

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
-
+const router = require('./routes/router');
 const server = express();
 
 server.use(express.json());
 
+server.use('/api/posts', router); 
+
+server.get('/', (req, res) => {
+    res.send(`Hello!`);
+});
 
 server.listen(4000, () => {
     console.log("\n*** Server Running on http://localhost:4000 ***\n");
