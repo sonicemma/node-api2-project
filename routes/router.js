ec97c262cb585315380bf92173c2f35cef34483b
emma <sonicjane2020@gmail.com>
2020-04-08 01:30:25 -0700

    done first two endposts, router get and router get by id

--- routes/router.js
+++ routes/router.js
@@ -3,15 +3,35 @@ const Posts = require('../data/db');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send(`hello from router`);
-})
+    Posts.find(req.query)
+    .then((posts) => {
+        res.status(200).json({queryString: req.query, posts})
+    })
+    .catch((error) => {
+        console.log(error.message, error);
+        res.status(500).json({
+            message: 'Error retrieving posts',
+        });
+    });
+});
 
 router.get('/:id', (req, res) => {
-    
+    Posts.findById(req.params.id)
+    .then((post) => {
+        if(post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({message: 'Post not found'})
+        }
+        }
+    )
+    .catch((err) => {
+        console.log(err.message, err);
+        res.status(500).json({message: 'Error retrieving post'})
+    })
 })
 
 router.get('/:id/comments', (req, res) => {
-    
 })
 
 router.post('/', (req, res) => {
